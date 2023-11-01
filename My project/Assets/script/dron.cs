2c19c27b08fe425b7e32a6aa77b406e8366bf6c4
flyingore1115 <133873619+flyingore1115@users.noreply.github.com>
2023-11-01 17:12:36 +0900

    S

--- My project/Assets/script/dron.cs
+++ My project/Assets/script/dron.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class gasi : MonoBehaviour
+public class dron : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
